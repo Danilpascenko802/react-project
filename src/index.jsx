fe68f31648908d4b5ae3d066e32c121e7c21a796
Danilpaschenko802 <Danilpaschenko802@gmail.com>
2022-09-29 18:57:59 +0200

    first steps with react

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+let title = "It works";
+console.log(title);
+
+title = document.getElementById(main_page_text);
+console.log(document.getElementsByClassName("mainpage_text"));
+let mainpt = document.getElementsByClassName("mainpage_text");
+console.log(mainpt);
+mainpt.innerHTML = "Title";
+console.log(mainpt.innerHTML);
